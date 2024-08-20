c39df7dcbed3d852a943346a8e2f970c369a1460
ashithprakas <ashithprakas@gmail.com>
2024-08-20 21:40:16 +0530

    Added hightLights section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,67 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useEffect, useState } from "react";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 700 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrc = () => {
+    if (window.innerWidth < 700) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrc);
+
+    return () => {
+      window.removeEventListener("resize", handleVideoSrc);
+    };
+  }, []);
+  useGSAP(() => {
+    gsap.to(".hero-title", {
+      opacity: 1,
+      delay: 2,
+    });
+
+    gsap.to("#callToAction", { opacity: 1, delay: 2, y: -40 });
+  }, []);
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+
+        <div className="md:w-10/12 w-9/12 ">
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} />
+          </video>
+        </div>
+
+        <div
+          id="callToAction"
+          className="flex flex-col items-center opacity-0 translate-y-20"
+        >
+          <a href="#hightlights" className="btn">
+            Buy
+          </a>
+          <p className="font-normal text-xl">From $199/month or $999</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
